a210f18e2d0b7e20fc8544769afd82714aecfd38
cesarbey16 <73254921+cesarbey16@users.noreply.github.com>
2026-04-18 00:01:51 +0300

    Refactor room management and add screen sharing
    update

--- server.js
+++ server.js
@@ -6,13 +6,12 @@ const io = require("socket.io")(http);
 app.use(express.static("public"));
 
 let rooms = {};
-let users = {};
 
 io.on("connection", socket => {
 
-  socket.on("createRoom", ({name, pass}) => {
+  socket.on("createRoom", ({name, pass})=>{
     if(!rooms[name]){
-      rooms[name] = {pass, users:[]};
+      rooms[name] = {pass, users:[], stream:false};
       updateRooms();
     }
   });
@@ -23,14 +22,19 @@ io.on("connection", socket => {
 
   socket.on("joinRoom", ({room, username, pass})=>{
     if(rooms[room] && rooms[room].pass === pass){
+
       socket.join(room);
       socket.room = room;
       socket.username = username;
 
-      rooms[room].users.push(username);
-      users[socket.id] = username;
+      rooms[room].users.push({id:socket.id, name:username});
 
-      io.to(room).emit("users", rooms[room].users);
+      io.to(room).emit("users", rooms[room].users.map(u=>u.name));
+
+      if(rooms[room].stream){
+        socket.emit("screenStarted",{id:null});
+      }
+
       updateRooms();
     }
   });
@@ -39,26 +43,39 @@ io.on("connection", socket => {
     io.to(socket.room).emit("message", {text:msg});
   });
 
-  socket.on("startScreen", ()=>{
-    socket.to(socket.room).emit("screenStarted");
+  /* WEBRTC */
+  socket.on("offer", ({offer, to})=>{
+    io.to(to).emit("offer", {offer, from:socket.id});
   });
 
-  socket.on("stopScreen", ()=>{
-    socket.to(socket.room).emit("screenStopped");
+  socket.on("answer", ({answer, to})=>{
+    io.to(to).emit("answer", {answer, from:socket.id});
+  });
+
+  socket.on("candidate", ({candidate, to})=>{
+    io.to(to).emit("candidate", {candidate, from:socket.id});
+  });
+
+  /* SCREEN */
+  socket.on("startScreen", ()=>{
+    if(socket.room){
+      rooms[socket.room].stream = true;
+      socket.to(socket.room).emit("screenStarted",{id:socket.id});
+    }
   });
 
   socket.on("disconnect", ()=>{
     if(socket.room && rooms[socket.room]){
       rooms[socket.room].users =
-        rooms[socket.room].users.filter(u=>u!==socket.username);
+        rooms[socket.room].users.filter(u=>u.id!==socket.id);
 
-      delete users[socket.id];
+      io.to(socket.room).emit("users", rooms[socket.room].users.map(u=>u.name));
       updateRooms();
     }
   });
 
   function updateRooms(target){
-    const data = Object.keys(rooms).map(r => ({
+    const data = Object.keys(rooms).map(r=>({
       name:r,
       count:rooms[r].users.length,
       locked:!!rooms[r].pass
@@ -66,14 +83,9 @@ io.on("connection", socket => {
 
     if(target) target.emit("rooms", data);
     else io.emit("rooms", data);
-
-    io.emit("onlineCount", Object.keys(users).length);
   }
 
 });
 
 const PORT = process.env.PORT || 3000;
-
-http.listen(PORT, () => {
-  console.log("Server çalışıyor:", PORT);
-});
+http.listen(PORT, ()=>console.log("Server çalışıyor:", PORT));
